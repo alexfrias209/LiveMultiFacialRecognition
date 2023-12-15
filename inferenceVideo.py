6adc284d9bd2a182edd0dc7675266f1067ffae2c
Alex Frias <77369012+alexfrias209@users.noreply.github.com>
2023-12-15 06:18:16 -0800

    Update inferenceVideo.py

--- inferenceVideo.py
+++ inferenceVideo.py
@@ -6,14 +6,14 @@ import numpy as np
 import torch.nn.functional as F
 from torchvision import transforms
 
-# Load the model
-def load_model(checkpoint_path, num_classes):
+# loading model
+def load_model(checkpoint_path, num_classes, device):
     model = InceptionResnetV1(classify=True, num_classes=num_classes).eval()
-    checkpoint = torch.load(checkpoint_path)
+    checkpoint = torch.load(checkpoint_path, map_location=device)
     model.load_state_dict(checkpoint['model_state_dict'])
     return model
 
-# Prepare face tensor for model input
+# face tensor for InceptionResnet input
 def prepare_face_tensor(face):
     trans = transforms.Compose([
         transforms.Resize((500, 500)),
@@ -22,19 +22,18 @@ def prepare_face_tensor(face):
     ])
     return trans(face).unsqueeze(0)
 
-# Determine if a GPU is available
+# device agnostic code
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 mtcnn = MTCNN(keep_all=True, device=device)
-model = load_model('./model_checkpoint_epoch_11.pth', num_classes=2)
+model = load_model('./model.pth', num_classes=2, device = device)
 model = model.to(device)
 classes = ["Alex", "Alexandra"]
 
-# Set the probability threshold for predictions
 probability_threshold = 0.8
 
-# Load video using OpenCV
+# loading video
 video_path = 'Test.mp4'
 cap = cv2.VideoCapture(video_path)
 
@@ -44,39 +43,39 @@ while cap.isOpened():
     if not ret:
         break
 
-    # Convert BGR (OpenCV format) to RGB for MTCNN
+    #converting BGR to RGB for MTCNN
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame_pil = Image.fromarray(frame_rgb)
 
-    # Detect faces
+    # detecting faces
     boxes, _ = mtcnn.detect(frame_pil)
 
     if boxes is not None:
         for box in boxes:
-            # Draw rectangle
+
             cv2.rectangle(frame, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 2)
 
-            # Predict class for each face
+            # predicting class for each face
             face = frame_pil.crop(box)
             face_tensor = prepare_face_tensor(face).to(device)
             prediction = model(face_tensor)
             probabilities = F.softmax(prediction, dim=1)
             top_probability, predicted_class = torch.max(probabilities, dim=1)
 
-            # Check if the top probability is above the threshold
+
             if top_probability.item() > probability_threshold:
                 label = f"{classes[predicted_class.item()]}: {top_probability.item():.2f}"
             else:
                 label = "Uncertain"
 
-            # Draw label using OpenCV
+            # drawing the labels
             cv2.putText(frame, label, (int(box[0]), int(box[1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
     frames_tracked.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
 cap.release()
 
-# Save the video with predictions
+# saving videos with all our drawn stuff
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('video_trackedF.mp4', fourcc, 25.0, (frames_tracked[0].shape[1], frames_tracked[0].shape[0]))
 for frame in frames_tracked:
